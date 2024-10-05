9d6abd05fafaceead789cdea1ba38a611750c8db
lazerusrm <lazerusrm@gmail.com>
2024-10-05 15:03:47 -0600

    deb

--- install.sh
+++ install.sh
@@ -11,7 +11,7 @@ apt-get update || { echo "apt-get update failed."; exit 1; }
 apt-get install -y apt-transport-https ca-certificates curl gnupg lsb-release || { echo "Dependency installation for Docker failed."; exit 1; }
 
 # Add Docker's repository GPG key
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg || { echo "Adding Docker GPG key failed."; exit 1; }
+curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg || { echo "Adding Docker GPG key failed."; exit 1; }
 
 # Add Docker's repository to sources
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null || { echo "Adding Docker repository to sources failed."; exit 1; }
