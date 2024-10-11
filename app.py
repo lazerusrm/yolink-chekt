30809c788a091f81f4ceccd07a75ab1464bee2e8
lazerusrm <lazerusrm@gmail.com>
2024-10-11 16:21:36 -0600

    Update app.py

--- app.py
+++ app.py
@@ -617,4 +617,9 @@ mqtt_thread.start()
 if __name__ == "__main__":
     load_config()
     
+    # Ensure Flask application context is active
+    with app.app_context():
+        refresh_and_save_devices()
+
     app.run(host='0.0.0.0', port=5000)
+
