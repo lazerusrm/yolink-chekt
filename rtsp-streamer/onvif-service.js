70ef9182cfaa59cc10d557f48ba6a2af60901dd5
lazerusrm <lazerusrm@gmail.com>
2025-03-03 11:03:09 -0700

    onvif stuff.

--- rtsp-streamer/onvif-service.js
+++ rtsp-streamer/onvif-service.js
@@ -1,190 +1,149 @@
-/**
- * ONVIF Service for YoLink Dashboard
- * Implements basic ONVIF device and media services
- */
-
-const { OnvifServer, Server } = require('node-onvif-server');
+const soap = require('soap');
+const dgram = require('dgram');
 const ip = require('ip');
 const uuid = require('uuid');
-const os = require('os');
-
-// Device information
-const deviceInfo = {
-  manufacturer: 'YoLink',
-  model: 'Dashboard-RTSP',
-  firmwareVersion: '1.0.0',
-  serialNumber: uuid.v4(),
-  hardwareId: 'YOLINK-DASHBOARD-1'
-};
 
 class OnvifService {
   constructor(config) {
     this.config = config;
-    this.server = null;
     this.rtspUrl = null;
-    this.deviceId = uuid.v4();
-    this.initialized = false;
+    this.serverIp = config.serverIp || ip.address();
+    this.onvifPort = config.onvifPort || 8555;
+    this.deviceInfo = {
+      manufacturer: 'YoLink',
+      model: 'Dashboard-RTSP',
+      firmwareVersion: '1.0.0',
+      serialNumber: uuid.v4(),
+      hardwareId: 'YOLINK-DASHBOARD-1'
+    };
   }
 
-  /**
-   * Initialize the ONVIF service
-   * @param {string} rtspUrl The RTSP URL to be exposed via ONVIF
-   */
   initialize(rtspUrl) {
-    if (this.initialized) return;
-
     this.rtspUrl = rtspUrl;
-    const serverPort = this.config.onvifPort || 8555;
-    const serverIp = this.config.serverIp || ip.address();
-
-    console.log(`Starting ONVIF server on ${serverIp}:${serverPort}`);
-
-    try {
-      // Create ONVIF server instance
-      this.server = new OnvifServer({
-        port: serverPort,
-        serviceAddress: `http://${serverIp}:${serverPort}/onvif/service`,
-        deviceInfo: {
-          manufacturer: deviceInfo.manufacturer,
-          model: deviceInfo.model,
-          firmwareVersion: deviceInfo.firmwareVersion,
-          serialNumber: deviceInfo.serialNumber,
-          hardwareId: deviceInfo.hardwareId
-        },
-        // Specify hostname and port for devices to connect to this server
-        hostname: serverIp,
-        port: serverPort
-      });
-
-      // Configure device information
-      this.server.setDeviceInformation({
-        Manufacturer: deviceInfo.manufacturer,
-        Model: deviceInfo.model,
-        FirmwareVersion: deviceInfo.firmwareVersion,
-        SerialNumber: deviceInfo.serialNumber,
-        HardwareId: deviceInfo.hardwareId
-      });
-
-      // Add media profile for the RTSP stream
-      this.addRtspMediaProfile();
-
-      // Start the server
-      this.server.start();
-      this.initialized = true;
-
-      console.log(`ONVIF server started successfully. Device available at: onvif://${serverIp}:${serverPort}`);
-      console.log(`RTSP stream registered: ${this.rtspUrl}`);
-
-      // Enable discovery
-      this.enableDiscovery();
-    } catch (error) {
-      console.error('Failed to start ONVIF server:', error);
-    }
+    this.startSoapServer();
+    this.startDiscovery();
+    console.log(`ONVIF service initialized: onvif://${this.serverIp}:${this.onvifPort}`);
   }
 
-  /**
-   * Add media profile for the RTSP stream
-   */
-  addRtspMediaProfile() {
-    if (!this.server) return;
-
-    try {
-      // Add a media profile for the RTSP stream
-      const profileToken = 'YolinkDashboardProfile';
-      const streamUri = this.rtspUrl;
-
-      // Add video source
-      const videoSourceToken = 'YolinkDashboardVideoSource';
-      this.server.addVideoSource(videoSourceToken, {
-        Framerate: this.config.frameRate || 1,
-        Resolution: {
-          Width: this.config.width || 1920,
-          Height: this.config.height || 1080
-        }
-      });
-
-      // Add video encoder configuration
-      const videoEncoderToken = 'YolinkDashboardVideoEncoder';
-      this.server.addVideoEncoderConfiguration(videoEncoderToken, {
-        Encoding: 'H264',
-        Resolution: {
-          Width: this.config.width || 1920,
-          Height: this.config.height || 1080
-        },
-        Quality: 6,
-        RateControl: {
-          FrameRateLimit: this.config.frameRate || 1,
-          EncodingInterval: 1,
-          BitrateLimit: 2048
-        },
-        H264: {
-          GovLength: 50,
-          H264Profile: 'Main'
-        }
-      });
-
-      // Create a media profile
-      this.server.addProfile(profileToken, {
-        name: 'YoLink Dashboard Stream',
-        videoSourceConfiguration: {
-          sourceToken: videoSourceToken,
-          bounds: {
-            x: 0,
-            y: 0,
-            width: this.config.width || 1920,
-            height: this.config.height || 1080
-          }
-        },
-        videoEncoderConfiguration: {
-          token: videoEncoderToken,
-          encoding: 'H264',
-          resolution: {
-            width: this.config.width || 1920,
-            height: this.config.height || 1080
+  startSoapServer() {
+    const service = {
+      DeviceService: {
+        DevicePort: {
+          GetDeviceInformation: (args, callback) => {
+            callback(null, {
+              Manufacturer: this.deviceInfo.manufacturer,
+              Model: this.deviceInfo.model,
+              FirmwareVersion: this.deviceInfo.firmwareVersion,
+              SerialNumber: this.deviceInfo.serialNumber,
+              HardwareId: this.deviceInfo.hardwareId
+            });
           },
-          quality: 6,
-          rateControl: {
-            frameRateLimit: this.config.frameRate || 1,
-            encodingInterval: 1,
-            bitrateLimit: 2048
-          },
-          H264: {
-            govLength: 50,
-            H264Profile: 'Main'
+          GetStreamUri: (args, callback) => {
+            callback(null, {
+              Uri: this.rtspUrl,
+              InvalidAfterConnect: false,
+              InvalidAfterReboot: false,
+              Timeout: 'PT0S'
+            });
           }
         }
-      });
+      }
+    };
 
-      // Set the stream URI for the profile
-      this.server.setStreamUri(profileToken, streamUri);
+    const wsdl = `
+      <?xml version="1.0" encoding="UTF-8"?>
+      <definitions xmlns="http://schemas.xmlsoap.org/wsdl/"
+                   xmlns:soap="http://schemas.xmlsoap.org/wsdl/soap/"
+                   targetNamespace="http://www.onvif.org/ver10/device/wsdl">
+        <message name="GetDeviceInformationRequest"/>
+        <message name="GetDeviceInformationResponse">
+          <part name="parameters" element="Manufacturer Model FirmwareVersion SerialNumber HardwareId"/>
+        </message>
+        <message name="GetStreamUriRequest"/>
+        <message name="GetStreamUriResponse">
+          <part name="parameters" element="Uri"/>
+        </message>
+        <portType name="DevicePort">
+          <operation name="GetDeviceInformation">
+            <input message="tns:GetDeviceInformationRequest"/>
+            <output message="tns:GetDeviceInformationResponse"/>
+          </operation>
+          <operation name="GetStreamUri">
+            <input message="tns:GetStreamUriRequest"/>
+            <output message="tns:GetStreamUriResponse"/>
+          </operation>
+        </portType>
+        <binding name="DeviceBinding" type="tns:DevicePort">
+          <soap:binding style="document" transport="http://schemas.xmlsoap.org/soap/http"/>
+          <operation name="GetDeviceInformation">
+            <soap:operation soapAction="http://www.onvif.org/GetDeviceInformation"/>
+            <input><soap:body use="literal"/></input>
+            <output><soap:body use="literal"/></output>
+          </operation>
+          <operation name="GetStreamUri">
+            <soap:operation soapAction="http://www.onvif.org/GetStreamUri"/>
+            <input><soap:body use="literal"/></input>
+            <output><soap:body use="literal"/></output>
+          </operation>
+        </binding>
+        <service name="DeviceService">
+          <port name="DevicePort" binding="tns:DeviceBinding">
+            <soap:address location="http://${this.serverIp}:${this.onvifPort}/onvif/device_service"/>
+          </port>
+        </service>
+      </definitions>
+    `;
 
-      console.log(`Added media profile: ${profileToken} with RTSP URI: ${streamUri}`);
-    } catch (error) {
-      console.error('Failed to add media profile:', error);
-    }
+    const soapServer = soap.listen(null, {
+      path: '/onvif/device_service',
+      port: this.onvifPort,
+      services: service,
+      xml: wsdl
+    });
+
+    soapServer.on('error', (err) => console.error('SOAP server error:', err));
+    console.log(`SOAP server running on http://${this.serverIp}:${this.onvifPort}/onvif/device_service`);
   }
 
-  /**
-   * Enable WS-Discovery for ONVIF device discovery
-   */
-  enableDiscovery() {
-    try {
-      // The node-onvif-server package should handle discovery automatically
-      console.log('ONVIF discovery service enabled');
-    } catch (error) {
-      console.error('Failed to enable ONVIF discovery:', error);
-    }
+  startDiscovery() {
+    const socket = dgram.createSocket('udp4');
+    socket.bind(3702, () => socket.setMulticastTTL(128));
+
+    socket.on('message', (msg, rinfo) => {
+      if (msg.toString().includes('Probe')) {
+        const response = `
+          <s:Envelope xmlns:s="http://www.w3.org/2003/05/soap-envelope" xmlns:a="http://schemas.xmlsoap.org/ws/2004/08/addressing">
+            <s:Header>
+              <a:Action>http://schemas.xmlsoap.org/ws/2005/04/discovery/ProbeMatches</a:Action>
+              <a:To>s:Sender</a:To>
+            </s:Header>
+            <s:Body>
+              <d:ProbeMatches xmlns:d="http://schemas.xmlsoap.org/ws/2005/04/discovery">
+                <d:ProbeMatch>
+                  <a:EndpointReference><a:Address>urn:uuid:${this.deviceInfo.serialNumber}</a:Address></a:EndpointReference>
+                  <d:Types>dn:NetworkVideoTransmitter</d:Types>
+                  <d:Scopes>onvif://www.onvif.org/name/YoLinkDashboard</d:Scopes>
+                  <d:XAddrs>http://${this.serverIp}:${this.onvifPort}/onvif/device_service</d:XAddrs>
+                </d:ProbeMatch>
+              </d:ProbeMatches>
+            </s:Body>
+          </s:Envelope>
+        `;
+        socket.send(response, 0, response.length, rinfo.port, rinfo.address);
+      }
+    });
+
+    socket.on('listening', () => console.log('WS-Discovery listening on UDP 3702'));
+    socket.on('error', (err) => console.error('Discovery error:', err));
+    this.discoverySocket = socket;
   }
 
-  /**
-   * Stop the ONVIF service
-   */
   stop() {
-    if (this.server) {
-      this.server.stop();
-      this.initialized = false;
-      console.log('ONVIF server stopped');
+    if (this.discoverySocket) {
+      this.discoverySocket.close();
+      console.log('WS-Discovery stopped');
     }
+    // SOAP server stop is handled by server shutdown in server.js
   }
 }
 
