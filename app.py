d63a1cccb63380c159428e6eff914c41cd04788a
lazerusrm <lazerusrm@gmail.com>
2025-03-09 13:03:55 -0600

    Update app.py

--- app.py
+++ app.py
@@ -136,11 +136,6 @@ async def init_default_user() -> None:
     except Exception as e:
         logger.error(f"Error creating default user: {e}")
 
-@app.route("/init_user_debug", methods=["GET"]) #Remove Before Production
-async def debug_init_user():
-    await init_default_user()
-    return jsonify({"status": "init triggered"})
-
 # ----------------------- Utility Functions -----------------------
 
 async def check_mqtt_connection_active() -> Dict[str, Any]:
